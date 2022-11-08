cabb1c25b096a7eaf6c5408a510bd64f975e7230
Egoushka <egorgrabovskij@gmail.com>
2022-11-08 23:16:19 +0200

    Added some basic stuff and tested connection to mongo

---
+++ BookApi/Models/DatabaseSettings.cs
@@ -0,0 +1,10 @@
+﻿namespace BookApi.Models;
+
+public class DatabaseSettings
+{
+    public string ConnectionString { get; set; } = null!;
+
+    public string DatabaseName { get; set; } = null!;
+
+    public string BooksCollectionName { get; set; } = null!;
+}
--- BookApi/Program.cs
+++ BookApi/Program.cs
@@ -1,3 +1,6 @@
+using BookApi.Models;
+using BookApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,7 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.Configure<DatabaseSettings>(
+    builder.Configuration.GetSection("BookStoreDatabase"));
+builder.Services.AddSingleton<BookService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
